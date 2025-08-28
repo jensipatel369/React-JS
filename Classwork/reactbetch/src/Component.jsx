8bf415d673f7b136436c0bcc012403096070f7e4
jensipatel369 <jensipatel369@gmail.com>
2025-08-28 11:49:32 +0530

    Prectice

---
+++ Classwork/reactbetch/src/Component.jsx
@@ -0,0 +1,18 @@
+// Component :- Component is a re-usable piece of code returns a part of UI.
+// -> There are 2 (two) type of component :-
+//    1. Functional Base Component
+//    2. Class Base Component
+
+//    1. Functional Base Component :- 
+
+// Module :- These are user define files or block of code which we can import and export.
+
+// JSX :- JavaScript XML it allows us to write HTML with JavaScript together. Represent by {}.
+
+function Component(){
+    return <div>
+        <h1>This is a Component.</h1>
+    </div>
+}
+
+export default Component;
