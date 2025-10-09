50980aad1257b0483c2279dfe188e062f50474d9
Jensi Patel <jensipatel369@gmail.com>
2025-10-09 10:17:33 +0530

    Prectice

--- Classwork/Routing/src/App.jsx
+++ Classwork/Routing/src/App.jsx
@@ -1,21 +1,17 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Home from './components/Home'
-import About from './components/About'
-import Read from './components/CRUD/Read'
-import Add from './components/CRUD/Add'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Read from "./Components/Read";
+import Add from "./Components/Add";
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
-        {/* <Navbar /> */}
         <Routes>
-          <Route path='/' Component={Read}></Route>
-          <Route path='/add' Component={Add}></Route>
+          <Route path="/" Component={Read}> </Route>
+          <Route path="/add" Component={Add}> </Route>
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
