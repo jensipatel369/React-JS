53001ca8517cc90b5db3d895bd7f5e56112320cd
Jensi Patel <jensipatel369@gmail.com>
2025-10-12 21:03:52 +0530

    Prectice

---
+++ Project/Navigator/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+export default function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route>
+            
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
