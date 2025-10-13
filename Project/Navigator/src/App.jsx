f29d1c45cd37f67ad5b33b40ea22144f96430252
Jensi Patel <jensipatel369@gmail.com>
2025-10-13 12:23:31 +0530

    Prectice

--- Project/Navigator/src/App.jsx
+++ Project/Navigator/src/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Nav from './components/Nav'
+import Cart from './pages/Cart'
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
+        <Nav />
         <Routes>
-          <Route>
-            
-          </Route>
+          <Route path='/' Component={Home}></Route>
+          <Route path='/cart' Component={Cart}></Route>
         </Routes>
       </BrowserRouter>
     </>
---
+++ Project/Navigator/src/pages/Cart.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react'
+
+export default function Cart() {
+  useEffect((id)=>{
+    
+  })
+
+  return (
+    <div>
+      <h1>Cart</h1>
+    </div>
+  )
+}
