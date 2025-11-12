3952316887d54e7996bb09f96d15bb1e8492d426
Jensi Patel <jensipatel369@gmail.com>
2025-11-12 18:11:29 +0530

    Prectice

--- Classwork/Redux/src/App.jsx
+++ Classwork/Redux/src/App.jsx
@@ -3,13 +3,15 @@ import Counter from './components/Counter'
 import { Provider } from 'react-redux'
 import { store } from './app/store'
 import CRUD from './components/CRUD'
+import ApiData from './components/ApiData'
 
 export default function App() {
   return (
     <div>
       <Provider store={store}>
         {/* <Counter /> */}
-        <CRUD/>
+        {/* <CRUD/> */}
+        <ApiData/>
       </Provider>
     </div>
   )
--- Classwork/Redux/src/app/store.jsx
+++ Classwork/Redux/src/app/store.jsx
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import Counter from "../feature/CounterSlice";
 import todo from "../feature/TodoSlice";
+import api from "../feature/ApiSlice";
 
 export const store = configureStore({
   reducer : {
     CounterKey : Counter,
-    TodoKey : todo
+    TodoKey : todo,
+    ApiKey : api
   }
 })
