af99ed4f31e422b8cdc99255aa4d19263d81a7bd
Jensi Patel <jensipatel369@gmail.com>
2025-11-28 11:15:09 +0530

    Prectice

--- Prectice/Firebase-task manager/src/components/Deskboard.jsx
+++ Prectice/Firebase-task manager/src/components/Deskboard.jsx
@@ -40,6 +40,7 @@ export default function Dashboard() {
     }))
     setRecord(newData)
   }
+
   const handleTask = async () => {
     const obj = { uid: userId, Task, priourity };
     if (editIndex == null) {
@@ -70,6 +71,7 @@ export default function Dashboard() {
     setPriourity(singleData.priourity)
     setEditIndex(id);
   }
+
   const handleLogout = async () => {
     await signOut(auth)
     navigate("/")
@@ -94,7 +96,7 @@ export default function Dashboard() {
             <input type="text" value={Task} onChange={(e) => setTask(e.target.value)} placeholder='Enter Your Task' name='task' className="md:col-span-2 px-4 py-3 bg-blue-50/40 border border-blue-200 rounded-lg text-gray-700 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-300" />
             <input type="text" value={priourity} onChange={(e) => setPriourity(e.target.value)} placeholder='Priority (High/Medium/Low)' name='priority' className="px-4 py-3 bg-pink-50/40 border border-pink-200 rounded-lg text-gray-700 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-pink-300" />
           </div>
-          <button onClick={handleTask} className="mt-4 w-full py-3 rounded-lg font-semibold text-white bg-gradient-to-r from-blue-400 to-pink-400 hover:scale-[1.02] duration-200 shadow-lg cursor-pointer">{editIndex ? "Update Task" : "Add Task"}</button>
+          <button onClick={handleTask} className="mt-4 w-full py-3 rounded-lg font-semibold text-white bg-gradient-to-r from-blue-400 to-pink-400 hover:scale-[1.02] duration-200 shadow-lg cursor-pointer">{editIndex == null ? "Update Task" : "Add Task"}</button>
         </div>
 
         <div className="bg-white shadow-xl rounded-2xl p-8 border border-blue-100">
