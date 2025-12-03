a62ebaf8f755bd92bc241ecf168ec8162071fcac
Jensi Patel <jensipatel369@gmail.com>
2025-12-03 11:10:30 +0530

    Prectice

--- Project/Firebase-task manager/firebaseConfig.js
+++ Project/Firebase-task manager/firebaseConfig.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { GoogleAuthProvider } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -20,5 +21,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app)
+const provider = new GoogleAuthProvider(app);
 
-export { auth, db };
+export { auth, db, provider };
